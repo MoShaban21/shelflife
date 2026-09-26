06411405ff0a2ecec5684fb48ec191e9c72024d5
unknown <mostafa.darsh2004@gmail.com>
2026-09-26 15:14:50 +0300

    Refactor Duplicate Overdue - checkout

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -41,6 +41,13 @@ class Loan extends Model
 
     public function isOverdue(): Attribute
     {
-        return Attribute::get(fn () => is_null($this->returned_at) && $this->due_date < now());
+        return Attribute::get(fn() => is_null($this->returned_at) && $this->due_date < now());
+    }
+    
+    public function scopeOverdue($query)
+    {
+        return $query
+            ->whereNull('returned_at')
+            ->where('due_date', '<', now());
     }
 }
